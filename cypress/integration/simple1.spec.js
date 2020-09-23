b8a326cb0d25f482ea6b7691503a1152c7b07389
Isaac Friedman <isaac@isaac-friedman.com>
2020-09-23 11:53:08 -0400

    Add price check test block

--- cypress/integration/simple1.spec.js
+++ cypress/integration/simple1.spec.js
@@ -13,5 +13,19 @@ describe("Browser Actions", () => {
 
   it("Should display correct number of results",  () => {
     cy.get('.product_pod').its('length').should('eq', 16);
+  });
+})
+
+describe("Checks the price of A Light in the Attic", () => {
+  it("Goes to the poetry section", () => {
+    cy.get('li').contains('Poetry').click();
+    cy.url().should('include', 'poetry');
+  });
+  it("Finds the book we want", () => {
+    cy.get('a').contains('A Light in the ...').click();
+    cy.url().should('include', 'a-light-in-the-attic_1000');
+  });
+  it("checks the price", () => {
+    cy.get('.price_color').contains('51.77') //Saves dealing with the currency symbol
   })
 })
