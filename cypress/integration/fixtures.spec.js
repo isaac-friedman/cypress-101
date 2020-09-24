4351abf1f4b9405305e2daf7d4f082a1a823a1f4
Isaac Friedman <isaac@isaac-friedman.com>
2020-09-24 11:14:22 -0400

    Demonstrate fixtures

---
+++ cypress/integration/fixtures.spec.js
@@ -0,0 +1,18 @@
+describe("Demonstrates fixtures", () => {
+  it("Visits the Login page", () => {
+    cy.visit('http://zero.webappsecurity.com/login.html');
+    cy.url().should('include', 'login.html');
+    cy.title().should('eq', 'Zero - Log in');
+  });
+
+  it("Tries to log in", () => {
+    cy.fixture('users').then(users => {
+      const name = users.name;
+      const pass = users.pass;
+      cy.get('#user_login').type(name);
+      cy.get('#user_password').type(pass);
+    });
+
+  });
+
+})
