fe7d4516bd6bd20d5bcc91572aa114408c237c93
Isaac Friedman <isaac@isaac-friedman.com>
2020-09-25 11:07:40 -0400

    Demonstrate screenshot

--- cypress/integration/screenshots.spec.js
+++ cypress/integration/screenshots.spec.js
@@ -1,9 +1,10 @@
-describe("Demonstrates how to take screenshots" () => {
+describe("Demonstrates how to take screenshots", () => {
   it('takes a full page screenshot', () => {
-
+    cy.visit('https://devexpress.github.io/testcafe/example')
+    cy.screenshot({capture: 'fullPage'})
   });
 
   it('screenshots a single element', () => {
-
+    cy.get('header').screenshot();
   })
 })
