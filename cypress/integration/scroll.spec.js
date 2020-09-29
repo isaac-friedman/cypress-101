1137eda984dcdfca65c5a901f68926c134ccf5a1
Isaac Friedman <isaac@isaac-friedman.com>
2020-09-29 11:13:13 -0400

    Demonstrate scroll function

---
+++ cypress/integration/scroll.spec.js
@@ -0,0 +1,9 @@
+describe('Scrolls down and back up on a page', () => {
+  it('Scrolls down and back up', () => {
+    cy.visit('https://devexpress.github.io/testcafe/example');
+    cy.get('#submit-button').scrollIntoView()
+    cy.wait(2000);
+    cy.get('h1').contains('Example').scrollIntoView()
+    cy.wait(2000);
+  })
+})
