7665c9543cb6ca3de2579bc9025682a3e8ac1bfd
Isaac Friedman <isaac@isaac-friedman.com>
2020-09-29 11:35:20 -0400

    Reads from a file

--- cypress/integration/data.spec.js
+++ cypress/integration/data.spec.js
@@ -1,5 +1,11 @@
 describe("File I/O in cypress", () => {
   it("Writes to a json file", () => {
     cy.writeFile("log.json", {"name": "Isaac", "age": 30})
-  })
+  });
+
+  it("Reads from a json file", () => {
+    cy.readFile("log.json") //verify data from last test
+      .its('age')
+      .should('eq', 30)
+    })
 })
