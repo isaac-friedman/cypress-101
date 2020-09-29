4507c2326110a8eba28fe406d43dcb5c6b74db1d
Isaac Friedman <isaac@isaac-friedman.com>
2020-09-29 13:10:24 -0400

    Classes

--- cypress/integration/class.spec.js
+++ cypress/integration/class.spec.js
@@ -1,8 +1,28 @@
 class BasePage {
-  //TODO
+  static visitHomePage() {
+    cy.visit("https://devexpress.github.io/testcafe/example")
+  }
+
+  static wait(ms) {
+    cy.wait(ms)
+  }
+}
+
+class HomePage extends BasePage {
+  static scrollToButton() {
+    cy.get('#submit-button').scrollIntoView()
+  }
+
+  static scrollToTop() {
+    cy.get('h1').contains('Example').scrollIntoView()
   }
 }
 
 describe("Abstraction with classes in cypress", () => {
-  //TODO
+  it("scrolls up and down a HomePage class", () => {
+    HomePage.visitHomePage()
+    HomePage.scrollToButton()
+    HomePage.wait(3000)
+    HomePage.scrollToTop()
+  })
 })
